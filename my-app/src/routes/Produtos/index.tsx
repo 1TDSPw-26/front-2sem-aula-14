a16f04c876d1273e5b0f5f23149a9aa966727655
alecarlosjesus <profalexandre.jesus@fiap.com.br>
2026-09-24 23:15:40 -0300

    feat : Link dinâmico para editarProdutos
    Signed-off-by: alecarlosjesus <profalexandre.jesus@fiap.com.br>

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,5 +1,8 @@
 import { useEffect, useState } from "react";
 import type { TipoProduto } from "../../types/types";
+import { Link } from "react-router";
+import { FaRegEdit as Editar} from "react-icons/fa";
+import { RiDeleteBinLine as Excluir} from "react-icons/ri";
 
 export default function Produtos() {
 
@@ -33,7 +36,37 @@ export default function Produtos() {
     return (
         <main>
             <h2>Produtos</h2>
-            
+            <table border={1} style={{width:"100%", borderCollapse:"collapse"}}>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>NOME</th>
+                        <th>PREÇO</th>
+                        <th>ESTOQUE</th>
+                        <th>AÇÕES</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {produtos.map( (p)=>(
+
+                            <tr key={p.id}>
+                                <td>{p.id}</td>
+                                <td>{p.nome}</td>
+                                <td>{p.preco}</td>
+                                <td>{p.estoque}</td>
+                                <td>
+                                    <Link to={`/editar-produtos/${p.id}`}> <Editar/> </Link>  | 
+                                    <Link to="#" onClick={()=> confirm("Deseja realmente excluir?")}> <Excluir/> </Link>  | 
+                                </td>
+                            </tr>
+                    ))}
+                </tbody>
+                <tfoot>
+                    <tr>
+                        <td colSpan={5}>Quantidade de Produtos: {produtos.length}</td>
+                    </tr>
+                </tfoot>
+            </table>
         </main>
     )
 }
