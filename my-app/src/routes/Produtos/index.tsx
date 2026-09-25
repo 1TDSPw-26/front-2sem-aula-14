74aeb9dccce9b820a0c3d6ce877a0feee6bf37d4
Gabriel-Oliveira0611 <gabriel.pereira.0694@outlook.com>
2026-09-24 22:24:37 -0300

    feat: Iniciando o processo de consumo de API rest com json-server em Produtos. Consumo ok.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,8 +1,35 @@
+import { useEffect, useState } from "react";
+import type { TipoProduto } from "../../types/types";
 
 export default function Produtos() {
-    return (
-        <main>
-            <h2>Produtos</h2>
-        </main>
-    )
+  const [produtos, setProduto] = useState<TipoProduto[]>([]);
+
+  useEffect(() => {
+    const carregarProdutos = async () => {
+      try {
+        const resposta = await fetch("http://localhost:3001/produtos");
+
+        if (!resposta.ok) {
+          throw new Error(
+            `Erro no fetch de resposta dos produtos: ${resposta.status} - ${resposta.statusText}`,
+          );
+        }
+
+        const data: TipoProduto[] = await resposta.json();
+        console.log(data);
+        setProduto(data)
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    carregarProdutos();
+  }, []);
+
+  return (
+    <main>
+      <h2>Produtos</h2>
+      
+    </main>
+  );
 }
