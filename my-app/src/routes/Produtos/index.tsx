76e21c9fda5632bc19b367186e05abf597f8f633
lucas-silva8077 <lucasfernando8077@gmail.com>
2026-09-24 23:06:33 -0300

    feat: Adicionando icones no botões.

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,12 +1,13 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import type { TipoProduto } from "../../types/types";
+import { Link } from "react-router/internal/react-server-client";
 
 export default function Produtos(){
 
     //Recipiciente onde irei guardar a lista de produtos
     const[produtos,setProdutos] = useState<TipoProduto[]>([]);
 
-    userEffect(() => {
+    useEffect(() => {
         
         const carregarProdutos = async () => {
             try {
@@ -34,6 +35,36 @@ export default function Produtos(){
     return(
         <div>
             <h2>Produtos</h2>
+            <table border={1} style={{width: "100%", borderCollapse: "collapse"}}>
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>PREÇO</th>
+                            <th>ESTOQUE</th>
+                            <th>AÇÕES</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {produtos.map( (p)=> (
+                            <tr key={p.id}>
+                                <td>{p.nome}</td>
+                                <td>{p.preco}</td>
+                                <td>{p.estoque}</td>
+                                <td> 
+                                    <Link to={`/editar-produtos/${p.id}`}>EDITAR</Link>| EXCLUIR
+                                    </td>
+                            </tr>
+
+                        ))}
+                    </tbody>
+                    <tfoot>
+                        <tr>
+                            <td colSpan={5}>Quantidade de Produtos: {produtos.length}</td>
+                        </tr>
+                    </tfoot>
+                    
+            </table>
         </div>
     )
 }
